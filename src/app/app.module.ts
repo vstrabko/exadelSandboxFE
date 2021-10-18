11b521b3997e6f2be0c77d7150dd6f05a608d30f
Olga Zhurova <82470486+ozhurova@users.noreply.github.com>
2021-10-18 22:02:39 +0300

    [25]-Added-angular-material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,13 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule, AppRoutingModule
+    BrowserModule, AppRoutingModule, BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
