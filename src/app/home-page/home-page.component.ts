9d5f32d776f745daf4f3e639ba4941a9c6772853
PaulRomanov <ramosha@mail.ru>
2021-10-20 18:33:24 +0300

    [31]-add-arr

--- src/app/home-page/home-page.component.ts
+++ src/app/home-page/home-page.component.ts
@@ -6,6 +6,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home-page.component.scss'],
 })
 export class HomePageComponent implements OnInit {
+  values = [
+    { id: 1, name: 'JS + .NET' },
+    { id: 2, name: 'JS + Java' },
+    { id: 3, name: 'DevOps' },
+    { id: 4, name: 'QA' },
+    { id: 5, name: 'JS + Java + QA' },
+    { id: 6, name: 'DevOps' },
+  ];
   // constructor() {}
   // ngOnInit() {}
   ngOnInit(): void {
