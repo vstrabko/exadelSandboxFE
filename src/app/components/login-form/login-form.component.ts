cd59f7810922aef59a942ad8222a9ee723d3844e
Nicolai <72386426+Nicolai-boiko@users.noreply.github.com>
2021-10-22 13:39:28 +0300

    Update login-form.component.ts
    repair router navigate

--- src/app/components/login-form/login-form.component.ts
+++ src/app/components/login-form/login-form.component.ts
@@ -1,5 +1,6 @@
 import { Component, Output, EventEmitter } from '@angular/core';
 import { FormControl, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login-form',
@@ -7,6 +8,7 @@ import { FormControl, Validators } from '@angular/forms';
   styleUrls: ['./login-form.component.scss'],
 })
 export class LoginFormComponent {
+  constructor(private router: Router){}
   @Output() modal: EventEmitter<boolean> = new EventEmitter<boolean>();
   public title = 'Log in';
   email = new FormControl('', [Validators.required, Validators.email]);
@@ -19,7 +21,7 @@ export class LoginFormComponent {
         password: this.password.value,
       };
       this.modal.emit(false);
-      Location.go('/sandbox')
+      this.router.navigateByUrl("/sandbox");
     }
   }
   cancel(): void {
