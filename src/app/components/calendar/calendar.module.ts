2406846b0b835cd21d1f83ae327d1f54ad8a2e2b
Nicolai <72386426+Nicolai-boiko@users.noreply.github.com>
2021-10-29 10:59:16 +0300

    [50]-add-calendar

--- src/app/components/calendar/calendar.module.ts
+++ src/app/components/calendar/calendar.module.ts
@@ -1,8 +1,8 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CalendarComponent } from './calendar.component';
-import {MatDatepickerModule} from '@angular/material/datepicker';
-import {MatCardModule} from '@angular/material/card';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatCardModule } from '@angular/material/card';
 
 @NgModule({
   declarations: [CalendarComponent],
