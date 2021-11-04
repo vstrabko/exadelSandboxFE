fad2440d6330e51a6896efa2b244c798978e37b6
PaulRomanov <ramosha@mail.ru>
2021-11-04 19:09:50 +0300

    [79]-add-pagination-and-cards

--- src/app/pages/sandbox-list-page/sandbox-list-page.component.ts
+++ src/app/pages/sandbox-list-page/sandbox-list-page.component.ts
@@ -1,14 +1,40 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { PageEvent } from '@angular/material/paginator';
 import { Router } from '@angular/router';
+import { Sandbox } from 'src/app/models/sandbox.model';
+import { SandboxService } from '../../services/sandbox-service';
 
 @Component({
   selector: 'app-sandbox-list-page',
   templateUrl: './sandbox-list-page.component.html',
   styleUrls: ['./sandbox-list-page.component.scss'],
 })
-export class SandboxListPageComponent {
-  constructor(private router: Router) {}
+export class SandboxListPageComponent implements OnInit {
+  length = 500;
+  pageSize = 10;
+  pageSizeOptions: number[] = [5, 10, 25, 100];
+  sandbox: Sandbox;
+  datasource: any[] = [];
+  sandboxArr: any[] = [];
+
+  constructor(private router: Router, private sandboxService: SandboxService) {}
   goCreateSandbox(): void {
     void this.router.navigateByUrl('/sandbox/create');
+  }
+
+  getCardSandbox(sandbox: Sandbox): void {
+    this.sandbox = sandbox;
+  }
+
+  onPageChanged(e: PageEvent): void {
+    const firstCut: number = e.pageIndex * e.pageSize;
+    const secondCut: number = firstCut + e.pageSize;
+    this.sandboxArr = this.datasource.slice(firstCut, secondCut);
+  }
+  ngOnInit(): void {
+    this.sandboxService.get().subscribe((data: any) => {
+      this.datasource = data;
+      this.sandboxArr = this.datasource.slice(0, this.pageSize);
+    });
   }
 }
--- src/app/pages/sandbox-list-page/sandbox-list-page.module.ts
+++ src/app/pages/sandbox-list-page/sandbox-list-page.module.ts
@@ -2,9 +2,18 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SandboxListPageComponent } from './sandbox-list-page.component';
 import { InternationalizationModule } from 'src/app/internationalization/internationalization.module';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatSelectModule } from '@angular/material/select';
+import { SandboxCardModule } from 'src/app/components/sandbox-card/sandbox-card.module';
 
 @NgModule({
-  imports: [CommonModule, InternationalizationModule],
+  imports: [
+    CommonModule,
+    InternationalizationModule,
+    MatSelectModule,
+    MatPaginatorModule,
+    SandboxCardModule,
+  ],
   declarations: [SandboxListPageComponent],
   exports: [SandboxListPageComponent],
 })
