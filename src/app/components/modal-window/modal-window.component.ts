1b3b91adc30b160bda619fb1617a86035159536c
”Mikhail <mikhail.hasilau@gmail.com>
2021-11-09 09:49:23 +0300

    modal service + login ESC

--- src/app/components/modal-window/modal-window.component.ts
+++ src/app/components/modal-window/modal-window.component.ts
@@ -1,18 +1,30 @@
-import { Component, HostListener, Input } from '@angular/core';
+import { Component, HostListener, Input, OnInit } from '@angular/core';
+import { ModalWindowService } from './modal-window.service';
 
 @Component({
   selector: 'app-modal-window',
   templateUrl: './modal-window.component.html',
   styleUrls: ['./modal-window.component.scss'],
 })
-export class ModalWindowComponent {
+export class ModalWindowComponent implements OnInit {
+  constructor(private modalWindowService: ModalWindowService) {}
+
+  public vision: boolean;
+
+  public ngOnInit(): void {
+    this.modalWindowService.visible.subscribe((value: boolean) => console.log(value));
+
+    // const closeModal = this.modalWindowService.visible.subscribe( value => console.log(value));
+  }
+
   @Input() title: string = '';
 
   @HostListener('window:keydown', ['$event'])
-  closeESC(event: KeyboardEvent): void {
+  hotKeys(event: KeyboardEvent): void {
     switch (event.keyCode) {
       case 27:
         console.log('esc');
+        this.modalWindowService.visible.next(false);
         break;
       case 9:
         event.preventDefault();
