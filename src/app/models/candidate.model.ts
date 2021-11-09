b4af83a533d15624b9af4d67ecf2933ca50d0e6e
Pavel <60575337+PaulRomanov@users.noreply.github.com>
2021-11-09 11:55:33 +0300

    [79]-fix-code

--- src/app/models/candidate.model.ts
+++ src/app/models/candidate.model.ts
@@ -5,7 +5,6 @@ export class Candidate extends ResourceModel<Candidate> {
   public name: string;
   public lastName: string;
   public email: string;
-  public id: number;
 
   constructor(model?: Partial<Candidate>) {
     super(model);
