ce6a2b58c6ae82a27e5f99b9b76744e84606395c
Olga Zhurova <82470486+ozhurova@users.noreply.github.com>
2021-11-10 11:53:50 +0300

    [102] added translation of the notification

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -3,7 +3,7 @@ import { Injectable } from '@angular/core';
 import { Router } from '@angular/router';
 
 import { Observable } from 'rxjs/internal/Observable';
-import { map } from 'rxjs/operators';
+import { catchError, map } from 'rxjs/operators';
 import { BehaviorSubject } from 'rxjs/internal/BehaviorSubject';
 
 import { UserService } from '../services/user.service';
@@ -12,6 +12,8 @@ import { ToastService } from 'src/app/services/toast.service';
 import { User } from '../models/user.model';
 import { environment } from 'src/environments/environment';
 import { authResponse } from '../interfaces/interfaces';
+
+import { TranslateService } from '@ngx-translate/core';
 
 @Injectable({ providedIn: 'root' })
 export class AuthService {
@@ -23,11 +25,13 @@ export class AuthService {
     private userService: UserService,
     private router: Router,
     private toastService: ToastService,
+    private translateService: TranslateService,
   ) {
     this.authSubject = new BehaviorSubject<User | null>(this.currentUser || null);
+    this.translateService = translateService;
   }
 
-  login(email: string, password: string): Observable<void> {
+  login(email: string, password: string): Observable<any> {
     return this.http
       .post<any>(`${environment.API_URL}/authorization/sign-in`, { email, password })
       .pipe(
@@ -39,18 +43,23 @@ export class AuthService {
               void this.router.navigate(['/users']);
               this.currentUser = user;
             });
-          } else {
-            this.toastService.showError(
-              'Please check carefully that all details are correct',
-              'Authentication error',
-            );
           }
+        }),
+      )
+      .pipe(
+        catchError((err: any): Observable<any> => {
+          this.toastService.showError(
+            this.translateService.instant('authTostr.text'),
+            this.translateService.instant('authTostr.title'),
+          );
+          return Observable.throw(err);
         }),
       );
   }
 
   public logout(): void {
     localStorage.removeItem('accessToken');
+    localStorage.removeItem('refreshTokent');
     this.authSubject.next(null);
     void this.router.navigate(['']);
   }
@@ -66,7 +75,7 @@ export class AuthService {
   private setToken(response: authResponse | null): void {
     if (response) {
       localStorage.setItem('accessToken', response.accessToken);
-      localStorage.setItem('refreshTokentT', response.refreshToken);
+      localStorage.setItem('refreshTokent', response.refreshToken);
     }
   }
 }
